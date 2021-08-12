6f46981120568eef5f17b9467080c0bb64cd1738
Renato Salzano <renato.salzano92@gmail.com>
2021-08-12 17:37:28 +0200

    small fix

--- src/language/strings.js
+++ src/language/strings.js
@@ -24,7 +24,7 @@ export const string = {
   },
   about: {
     en: "Nerd since birth, at 11 I assembled my first computer, since then, I have learned to use different self-taught software.\nAfter careful consideration, I decided to approach web development, starting with FreeCodeCamp, quickly obtaining the certificate in responsive-web-design.\nI recently finished the Star2Impact course as a Front End Developer",
-    it: "Smanettone fin dalla nascita, ad 11 anni mi sono assemblato il mio primo computer, da allora, ho imparato ad utilizzare diversi software da autodidatta. \nDopo un'attenta riflessione, ho deciso di avvicinarmi allo sviluppo web, cominciando con FreeCodeCamp, ottenendo in breve tempo il certificato in responsive-web-design. \nRecentemente ho terminato il percorso su Star2Impact come Front End Developer",
+    it: "Appassionato di tecnologia fin dalla nascita, ad 11 anni mi sono assemblato il mio primo computer, da allora, ho imparato ad utilizzare diversi software da autodidatta. \nDopo un'attenta riflessione, ho deciso di avvicinarmi allo sviluppo web, cominciando con FreeCodeCamp, ottenendo in breve tempo il certificato in responsive-web-design. \nRecentemente ho terminato il percorso su Star2Impact come Front End Developer",
   },
   lang_string: {
     en: "Language",
